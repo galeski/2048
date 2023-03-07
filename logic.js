aec770db3b5dd01c31b626805af0e8f5cbe470c7
galeski <mgaleski@gmail.com>
2023-03-07 21:33:14 +0100

    Update

--- logic.js
+++ logic.js
@@ -1,5 +1,6 @@
 let currentState = undefined;
 let debug = false;
+let score = 1;
 
 const initializeGame = () => {
   const fields = new Array(4).fill(0).map(() => new Array(4).fill(0));
@@ -70,8 +71,48 @@ const startingState = () => {
   return game;
 };
 
-// TODO: add logic
-const checkIfSolvable = (gameState) => {};
+// TODO: refactor this
+const checkIfSolvable = (gameState) => {
+  let didWeLoseYet = 8;
+  for (let row = 0; row < gameState.length; row++) {
+    if (
+      gameState[row][0] !== gameState[row][1] &&
+      gameState[row][1] !== gameState[row][2] &&
+      gameState[row][2] !== gameState[row][3] &&
+      gameState[row][0] !== 0 &&
+      gameState[row][1] !== 0 &&
+      gameState[row][2] !== 0 &&
+      gameState[row][3] !== 0
+    ) {
+      didWeLoseYet--;
+    }
+  }
+
+  for (let row = 0; row < gameState.length; row++) {
+    for (let col = 0; col < gameState[row].length; col++) {
+      console.log(currentState[col][row]);
+      if (
+        gameState[0][row] !== 0 &&
+        gameState[1][row] !== 0 &&
+        gameState[2][row] !== 0 &&
+        gameState[3][row] !== 0 &&
+        gameState[0][row] !== gameState[1][row] &&
+        gameState[1][row] !== gameState[2][row] &&
+        gameState[2][row] !== gameState[3][row]
+      ) {
+        didWeLoseYet--;
+      }
+    }
+  }
+
+  if (didWeLoseYet === -12) {
+    document.getElementById("message").innerHTML = "You lost. Try again!";
+  }
+
+  if (gameState.flat().indexOf(2048) !== -1) {
+    document.getElementById("message").innerHTML = "You won the game!";
+  }
+};
 
 const addRandomNumber = (gameState) => {
   let zeroValues = [];
@@ -88,35 +129,32 @@ const addRandomNumber = (gameState) => {
 
   const randomField = randomFromArr(zeroValues);
 
+  console.log(randomField);
+
   if (randomField === undefined) {
     return gameState;
   }
   const row = randomField[0];
   const col = randomField[1];
   gameState[row][col] = 2;
-  console.log(typeof acc);
-  // FIXME: broken
-  // changeColor(String(acc));
-  console.log(randomField, acc);
 
-  // // FIXME: duplication of logic
-  // acc = 0;
-  // for (let row = 0; row < gameState.length; row++) {
-  //   for (let col = 0; col < gameState[0].length; col++) {
-  //     if (acc === randomField) {
-  //       gameState[row][col] = 2;
-  //       console.log(row, col, acc);
-  //       changeColor(String(acc));
-  //     }
-  //     acc++;
-  //   }
-  // }
+  // FIXME: just plain awful
+  acc = 0;
+  for (let row2 = 0; row2 < gameState.length; row2++) {
+    for (let col2 = 0; col2 < gameState[0].length; col2++) {
+      if (row2 === row && col2 === col) {
+        changeColor(acc);
+        break;
+      }
+      acc++;
+    }
+  }
 
   return acc;
 };
 
 const changeColor = (element, color = "lightblue") => {
-  document.getElementById(element).style.background = color;
+  document.getElementById(String(element)).style.background = color;
   setTimeout(function () {
     changeColor(element, "#fff");
   }, 1000);
@@ -171,31 +209,6 @@ const moveZeroes = (arr, right = false) => {
     arr[i] = fieldsWithZeroesAtEnd[i];
   }
 
-  // if (!right) {
-  //   count = 0;
-  //   for (let i = 0; i < arr.length; i++) {
-  //     if (arr[i] !== 0) {
-  //       arr[count++] = arr[i];
-  //     }
-  //   }
-
-  //   for (let i = count; i < arr.length; i++) {
-  //     arr[i] = 0;
-  //   }
-  // } else {
-  //   count = arr.length - 1;
-  //   for (let i = arr.length - 1; i >= 0; i--) {
-  //     if (arr[i] !== 0) {
-  //       arr[count--] = arr[i];
-  //     }
-  //   }
-
-  //   for (let i = 0; i < count; i++) {
-  //     arr[i] = 0;
-  //   }
-  //   return arr.reverse();
-  // }
-
   return arr;
 };
 
@@ -236,34 +249,6 @@ const solveField = (gameStateField, direction = null) => {
       gameStateField[1] = 0;
     }
   }
-
-  // if (gameStateField[0] === gameStateField[1]) {
-  //   gameStateField[0] = gameStateField[0] + gameStateField[1];
-  //   gameStateField[1] = 0;
-  // }
-  // if (gameStateField[1] === gameStateField[2]) {
-  //   gameStateField[1] = gameStateField[1] + gameStateField[2];
-  //   gameStateField[2] = 0;
-  // }
-  // if (gameStateField[2] === gameStateField[3]) {
-  //   gameStateField[2] = gameStateField[2] + gameStateField[3];
-  //   gameStateField[3] = 0;
-  //   console.log(gameStateField[2], gameStateField[3]);
-  // }
-
-  // // TODO: squashing ? ie [2, 0, 0, 2] -> move left = [4, 0, 0, 0]
-  // if (gameStateField[0] === gameStateField[2] && gameStateField[1] === 0) {
-  //   gameStateField[0] = gameStateField[0] + gameStateField[2];
-  //   gameStateField[2] = 0;
-  // }
-  // if (
-  //   gameStateField[0] === gameStateField[3] &&
-  //   gameStateField[1] === 0 &&
-  //   gameStateField[2] === 0
-  // ) {
-  //   gameStateField[0] = gameStateField[0] + gameStateField[3];
-  //   gameStateField[3] = 0;
-  // }
 
   return gameStateField;
 };
@@ -315,10 +300,14 @@ const move = (gameState, direction) => {
   if (prevState !== JSON.stringify(gameState)) {
     console.log("fields are different");
     addRandomNumber(gameState);
+    score += 2;
+    document.getElementById("message").innerHTML = String(score);
     updateDivs(gameState);
   } else {
     console.log("fields are the same, no movement");
   }
+
+  checkIfSolvable(gameState);
 };
 
 const checkKey = (e) => {
@@ -344,6 +333,9 @@ const checkKey = (e) => {
 
   console.log(e.keyCode);
 };
+
+window.addEventListener("touchstart", handleTouchStart, false);
+window.addEventListener("touchmove", handleTouchMove, false);
 
 window.addEventListener("load", (event) => {
   console.log("page is fully loaded");
@@ -375,6 +367,57 @@ window.onload = function () {
   };
 };
 
+/* https://stackoverflow.com/questions/2264072/detect-a-finger-swipe-through-javascript-on-the-iphone-and-android */
+let xDown = null;
+let yDown = null;
+
+function getTouches(evt) {
+  return (
+    evt.touches || // browser API
+    evt.originalEvent.touches
+  ); // jQuery
+}
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) {
+    return;
+  }
+
+  let xUp = evt.touches[0].clientX;
+  let yUp = evt.touches[0].clientY;
+
+  let xDiff = xDown - xUp;
+  let yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    /*most significant*/
+    if (xDiff > 0) {
+      /* right swipe */
+      move(currentState, "right");
+    } else {
+      /* left swipe */
+      move(currentState, "left");
+    }
+  } else {
+    if (yDiff > 0) {
+      /* down swipe */
+      move(currentState, "down");
+    } else {
+      /* up swipe */
+      move(currentState, "up");
+    }
+  }
+  /* reset values */
+  xDown = null;
+  yDown = null;
+}
+
 // some tests
 function runLeft() {
   setField(currentState, 0, 0, 2);
@@ -396,7 +439,6 @@ function runLeft() {
   setField(currentState, 3, 1, 32);
   setField(currentState, 3, 2, 32);
   setField(currentState, 3, 3, 32);
-  // move(currentState, "left");
 }
 
 function runRight() {
@@ -419,51 +461,84 @@ function runRight() {
   setField(currentState, 3, 1, 0);
   setField(currentState, 3, 2, 0);
   setField(currentState, 3, 3, 64);
-  // move(currentState, "left");
 }
 
 function test3() {
-  // setField(currentState, 0, 0, 8);
-  // setField(currentState, 0, 1, 8);
-  // setField(currentState, 0, 2, 8);
-  // setField(currentState, 0, 3, 8);
-
-  // setField(currentState, 1, 0, 0);
-  // setField(currentState, 1, 1, 8);
-  // setField(currentState, 1, 2, 32);
-  // setField(currentState, 1, 3, 8);
-
-  // setField(currentState, 2, 0, 0);
-  // setField(currentState, 2, 1, 32);
-  // setField(currentState, 2, 2, 64);
-  // setField(currentState, 2, 3, 32);
-
   setField(currentState, 3, 0, 2);
   setField(currentState, 3, 1, 0);
   setField(currentState, 3, 2, 2);
   setField(currentState, 3, 3, 0);
-  // move(currentState, "left");
 }
 
 function test4() {
-  // setField(currentState, 0, 0, 8);
-  // setField(currentState, 0, 1, 8);
-  // setField(currentState, 0, 2, 8);
-  // setField(currentState, 0, 3, 8);
-
-  // setField(currentState, 1, 0, 0);
-  // setField(currentState, 1, 1, 8);
-  // setField(currentState, 1, 2, 32);
-  // setField(currentState, 1, 3, 8);
-
-  // setField(currentState, 2, 0, 0);
-  // setField(currentState, 2, 1, 32);
-  // setField(currentState, 2, 2, 64);
-  // setField(currentState, 2, 3, 32);
-
   setField(currentState, 3, 0, 0);
   setField(currentState, 3, 1, 2);
   setField(currentState, 3, 2, 2);
   setField(currentState, 3, 3, 2);
-  // move(currentState, "left");
+}
+
+function testLost() {
+  setField(currentState, 0, 0, 2);
+  setField(currentState, 0, 1, 4);
+  setField(currentState, 0, 2, 2);
+  setField(currentState, 0, 3, 4);
+
+  setField(currentState, 1, 0, 4);
+  setField(currentState, 1, 1, 2);
+  setField(currentState, 1, 2, 4);
+  setField(currentState, 1, 3, 2);
+
+  setField(currentState, 2, 0, 2);
+  setField(currentState, 2, 1, 4);
+  setField(currentState, 2, 2, 2);
+  setField(currentState, 2, 3, 4);
+
+  setField(currentState, 3, 0, 4);
+  setField(currentState, 3, 1, 2);
+  setField(currentState, 3, 2, 4);
+  setField(currentState, 3, 3, 2);
+}
+
+function testLost2() {
+  setField(currentState, 0, 0, 2);
+  setField(currentState, 0, 1, 4);
+  setField(currentState, 0, 2, 2);
+  setField(currentState, 0, 3, 4);
+
+  setField(currentState, 1, 0, 8);
+  setField(currentState, 1, 1, 16);
+  setField(currentState, 1, 2, 8);
+  setField(currentState, 1, 3, 16);
+
+  setField(currentState, 2, 0, 32);
+  setField(currentState, 2, 1, 64);
+  setField(currentState, 2, 2, 32);
+  setField(currentState, 2, 3, 64);
+
+  setField(currentState, 3, 0, 4);
+  setField(currentState, 3, 1, 2);
+  setField(currentState, 3, 2, 4);
+  setField(currentState, 3, 3, 2);
+}
+
+function testWonGame() {
+  setField(currentState, 0, 0, 2048);
+  setField(currentState, 0, 1, 4);
+  setField(currentState, 0, 2, 2);
+  setField(currentState, 0, 3, 4);
+
+  setField(currentState, 1, 0, 8);
+  setField(currentState, 1, 1, 16);
+  setField(currentState, 1, 2, 8);
+  setField(currentState, 1, 3, 16);
+
+  setField(currentState, 2, 0, 32);
+  setField(currentState, 2, 1, 64);
+  setField(currentState, 2, 2, 32);
+  setField(currentState, 2, 3, 64);
+
+  setField(currentState, 3, 0, 4);
+  setField(currentState, 3, 1, 2);
+  setField(currentState, 3, 2, 4);
+  setField(currentState, 3, 3, 0);
 }
